7a4b93972771a59480aed48d3aeb971df6a72a06
nikolayvf <nikolayvf@gmail.com>
2024-04-16 22:58:37 +0300

    Initial Commit

---
+++ LOTRConcerter17/LOTRConcerter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConcerter17App.swift
+//  LOTRConcerter17
+//
+//  Created by Nikolay Filipov on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConcerter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
